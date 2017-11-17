980d4518cd98dfea085e090275dd96cb3e54dad8
Anders Damsgaard <andersd@riseup.net>
2017-11-17 17:11:20 -0500

    remove build.jl and instead install at the beginning of Granular.jl

--- src/Granular.jl
+++ src/Granular.jl
@@ -1,5 +1,17 @@
 #!/usr/bin/env julia
 
+if is_apple() 
+    using Homebrew
+    Homebrew.add("gnuplot")
+    Homebrew.add("imagemagick")
+end
+
+if is_windows() 
+    using WinRPM
+    WinRPM.install("gnuplot")
+    WinRPM.install("imagemagick")
+end
+
 """
 # Granular.jl
 Offline granular dynamics simulator module.
