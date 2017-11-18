bec7677dc0ef0a4bd5f91faa495d3751de43e62e
Anders Damsgaard <andersd@riseup.net>
2017-11-17 19:18:34 -0500

    update package managers before installing

--- deps/build.jl
+++ deps/build.jl
@@ -3,12 +3,14 @@ using Compat
 
 if is_apple() 
     using Homebrew
+    Homebrew.update()
     Homebrew.add("gnuplot")
     Homebrew.add("imagemagick")
 end
 
 if is_windows() 
     using WinRPM
+    WinRPM.update()
     WinRPM.install("gnuplot")
     WinRPM.install("imagemagick")
 end
