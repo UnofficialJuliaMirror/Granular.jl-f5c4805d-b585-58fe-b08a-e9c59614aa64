ba27e7cf2151993e2d59aa43b13401a7aea19e5d
Anders Damsgaard <andersd@riseup.net>
2018-01-02 13:55:24 -0500

    Use cross-version compatible OS checks

--- src/io.jl
+++ src/io.jl
@@ -5,15 +5,16 @@ if typeof(Pkg.installed("JLD")) == VersionNumber
     import JLD
     hasJLD = true
 end
+import Compat
 using Compat.DelimitedFiles
 using Compat.Dates
 
 # load Homebrew/WinRPM for gnuplot and imagemagick
-if is_apple() 
+if Compat.Sys.isapple()
     using Homebrew
 end
 
-if is_windows() 
+if Compat.Sys.iswindows()
     using WinRPM
 end
 
