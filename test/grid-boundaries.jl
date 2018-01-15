cfbc4ba624fda848ca9576f1db813e411747a471
Anders Damsgaard <andersd@riseup.net>
2018-01-15 10:39:23 -0500

    Skip redirection of STDOUT in grid test on windows, which stopped working

--- test/grid-boundaries.jl
+++ test/grid-boundaries.jl
@@ -14,96 +14,98 @@ ocean = Granular.createEmptyOcean()
 @test ocean.bc_north == 1
 @test ocean.bc_south == 1
 
-const originalSTDOUT = STDOUT
-(out_r, out_w) = redirect_stdout()
-Granular.reportGridBoundaryConditions(ocean)
-close(out_w)
-redirect_stdout(originalSTDOUT)
-output = String(readavailable(out_r))
-@test output == """West  (-x): inactive\t(1)
-East  (+x): inactive\t(1)
-South (-y): inactive\t(1)
-North (+y): inactive\t(1)
-"""
+if !is_windows()
+    const originalSTDOUT = STDOUT
+    (out_r, out_w) = redirect_stdout()
+    Granular.reportGridBoundaryConditions(ocean)
+    close(out_w)
+    redirect_stdout(originalSTDOUT)
+    output = String(readavailable(out_r))
+    @test output == """West  (-x): inactive\t(1)
+    East  (+x): inactive\t(1)
+    South (-y): inactive\t(1)
+    North (+y): inactive\t(1)
+    """
 
-(out_r, out_w) = redirect_stdout()
-Granular.setGridBoundaryConditions!(ocean, "periodic", "south, west", verbose=true)
-close(out_w)
-redirect_stdout(originalSTDOUT)
-output = String(readavailable(out_r))
-@test output == """West  (-x): periodic\t(2)
-East  (+x): inactive\t(1)
-South (-y): periodic\t(2)
-North (+y): inactive\t(1)
-"""
-@test ocean.bc_west == 2
-@test ocean.bc_east == 1
-@test ocean.bc_north == 1
-@test ocean.bc_south == 2
+    (out_r, out_w) = redirect_stdout()
+    Granular.setGridBoundaryConditions!(ocean, "periodic", "south, west",
+                                        verbose=true)
+    close(out_w)
+    redirect_stdout(originalSTDOUT)
+    output = String(readavailable(out_r))
+    @test output == """West  (-x): periodic\t(2)
+    East  (+x): inactive\t(1)
+    South (-y): periodic\t(2)
+    North (+y): inactive\t(1)
+    """
+    @test ocean.bc_west == 2
+    @test ocean.bc_east == 1
+    @test ocean.bc_north == 1
+    @test ocean.bc_south == 2
 
-(out_r, out_w) = redirect_stdout()
-Granular.setGridBoundaryConditions!(ocean, "inactive", "all", verbose=false)
-close(out_w)
-redirect_stdout(originalSTDOUT)
-output = String(readavailable(out_r))
-@test output == ""
-@test ocean.bc_west == 1
-@test ocean.bc_east == 1
-@test ocean.bc_north == 1
-@test ocean.bc_south == 1
+    (out_r, out_w) = redirect_stdout()
+    Granular.setGridBoundaryConditions!(ocean, "inactive", "all", verbose=false)
+    close(out_w)
+    redirect_stdout(originalSTDOUT)
+    output = String(readavailable(out_r))
+    @test output == ""
+    @test ocean.bc_west == 1
+    @test ocean.bc_east == 1
+    @test ocean.bc_north == 1
+    @test ocean.bc_south == 1
 
-(out_r, out_w) = redirect_stdout()
-Granular.setGridBoundaryConditions!(ocean, "periodic", "all")
-close(out_w)
-output = String(readavailable(out_r))
-redirect_stdout(originalSTDOUT)
-@test output == """West  (-x): periodic\t(2)
-East  (+x): periodic\t(2)
-South (-y): periodic\t(2)
-North (+y): periodic\t(2)
-"""
-@test ocean.bc_west == 2
-@test ocean.bc_east == 2
-@test ocean.bc_north == 2
-@test ocean.bc_south == 2
+    (out_r, out_w) = redirect_stdout()
+    Granular.setGridBoundaryConditions!(ocean, "periodic", "all")
+    close(out_w)
+    output = String(readavailable(out_r))
+    redirect_stdout(originalSTDOUT)
+    @test output == """West  (-x): periodic\t(2)
+    East  (+x): periodic\t(2)
+    South (-y): periodic\t(2)
+    North (+y): periodic\t(2)
+    """
+    @test ocean.bc_west == 2
+    @test ocean.bc_east == 2
+    @test ocean.bc_north == 2
+    @test ocean.bc_south == 2
 
-(out_r, out_w) = redirect_stdout()
-Granular.setGridBoundaryConditions!(ocean, "inactive")
-close(out_w)
-output = String(readavailable(out_r))
-redirect_stdout(originalSTDOUT)
-@test output == """West  (-x): inactive\t(1)
-East  (+x): inactive\t(1)
-South (-y): inactive\t(1)
-North (+y): inactive\t(1)
-"""
-@test ocean.bc_west == 1
-@test ocean.bc_east == 1
-@test ocean.bc_north == 1
-@test ocean.bc_south == 1
+    (out_r, out_w) = redirect_stdout()
+    Granular.setGridBoundaryConditions!(ocean, "inactive")
+    close(out_w)
+    output = String(readavailable(out_r))
+    redirect_stdout(originalSTDOUT)
+    @test output == """West  (-x): inactive\t(1)
+    East  (+x): inactive\t(1)
+    South (-y): inactive\t(1)
+    North (+y): inactive\t(1)
+    """
+    @test ocean.bc_west == 1
+    @test ocean.bc_east == 1
+    @test ocean.bc_north == 1
+    @test ocean.bc_south == 1
 
-(out_r, out_w) = redirect_stdout()
-Granular.setGridBoundaryConditions!(ocean, "periodic")
-close(out_w)
-output = String(readavailable(out_r))
-redirect_stdout(originalSTDOUT)
-@test output == """West  (-x): periodic\t(2)
-East  (+x): periodic\t(2)
-South (-y): periodic\t(2)
-North (+y): periodic\t(2)
-"""
-@test ocean.bc_west == 2
-@test ocean.bc_east == 2
-@test ocean.bc_north == 2
-@test ocean.bc_south == 2
+    (out_r, out_w) = redirect_stdout()
+    Granular.setGridBoundaryConditions!(ocean, "periodic")
+    close(out_w)
+    output = String(readavailable(out_r))
+    redirect_stdout(originalSTDOUT)
+    @test output == """West  (-x): periodic\t(2)
+    East  (+x): periodic\t(2)
+    South (-y): periodic\t(2)
+    North (+y): periodic\t(2)
+    """
+    @test ocean.bc_west == 2
+    @test ocean.bc_east == 2
+    @test ocean.bc_north == 2
+    @test ocean.bc_south == 2
 
-@test_throws ErrorException Granular.setGridBoundaryConditions!(ocean,
-                                                                     "periodic",
-                                                                     "asdf")
+    @test_throws ErrorException Granular.setGridBoundaryConditions!(ocean,
+                                                                    "periodic",
+                                                                    "asdf")
 
-@test_throws ErrorException Granular.setGridBoundaryConditions!(ocean,
-                                                                     "asdf")
-
+    @test_throws ErrorException Granular.setGridBoundaryConditions!(ocean,
+                                                                    "asdf")
+end
 
 info("Testing granular interaction across periodic boundaries")
 sim = Granular.createSimulation()
