3c3b0f50604fe20bb5fcae0438ff15151e96d1e9
Anders Damsgaard <andersd@riseup.net>
2018-04-02 15:28:17 -0400

    Manually fix repeat commands across Julia 0.6 and 0.7

--- src/atmosphere.jl
+++ src/atmosphere.jl
@@ -136,22 +136,42 @@ function createRegularAtmosphereGrid(n::Vector{Int},
                                      bc_east::Integer = 1,
                                      bc_north::Integer = 1)
 
-    xq = Compat.repeat(Compat.range(origo[1], stop=origo[1] + L[1],
-                                    length=n[1] + 1),
-                       1, n[2] + 1)
-    yq = Compat.repeat(Compat.range(origo[2], stop=origo[2] + L[2],
-                                    length=n[2] + 1)',
-                       n[1] + 1, 1)
+    if VERSION < v"0.7.0-DEV"
+        xq = repmat(Compat.range(origo[1], stop=origo[1] + L[1],
+                                 length=n[1] + 1),
+                    1, n[2] + 1)
+        yq = repmat(Compat.range(origo[2], stop=origo[2] + L[2],
+                                 length=n[2] + 1)',
+                    n[1] + 1, 1)
+    else
+        xq = Compat.repeat(Compat.range(origo[1], stop=origo[1] + L[1],
+                                        length=n[1] + 1),
+                           1, n[2] + 1)
+        yq = Compat.repeat(Compat.range(origo[2], stop=origo[2] + L[2],
+                                        length=n[2] + 1)',
+                           n[1] + 1, 1)
+    end
 
     dx = L./n
-    xh = Compat.repeat(Compat.range(origo[1] + .5*dx[1],
-                                    stop=origo[1] + L[1] - .5*dx[1],
-                                    length=n[1]),
-                       1, n[2])
-    yh = Compat.repeat(Compat.range(origo[2] + .5*dx[2],
-                                    stop=origo[1] + L[2] - .5*dx[2],
-                                    length=n[2])',
-                       n[1], 1)
+    if VERSION < v"0.7.0-DEV"
+        xh = repmat(Compat.range(origo[1] + .5*dx[1],
+                                 stop=origo[1] + L[1] - .5*dx[1],
+                                 length=n[1]),
+                    1, n[2])
+        yh = repmat(Compat.range(origo[2] + .5*dx[2],
+                                 stop=origo[1] + L[2] - .5*dx[2],
+                                 length=n[2])',
+                    n[1], 1)
+    else
+        xh = Compat.repeat(Compat.range(origo[1] + .5*dx[1],
+                                        stop=origo[1] + L[1] - .5*dx[1],
+                                        length=n[1]),
+                           1, n[2])
+        yh = Compat.repeat(Compat.range(origo[2] + .5*dx[2],
+                                        stop=origo[1] + L[2] - .5*dx[2],
+                                        length=n[2])',
+                           n[1], 1)
+    end
 
     zl = -Compat.range(.5*dx[3], stop=L[3] - .5*dx[3], length=n[3])
 
